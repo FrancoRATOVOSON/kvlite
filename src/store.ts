bed19c57fd10d01fd2b0c419dc699654701cf261
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2025-02-15 21:57:13 +0300

    feat: update store

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,10 @@
-import { SQLiteExecutor } from './types'
+import { ParamsValueType, SQLiteExecutorObject } from './types'
 
 export default class Store {
-  private executor: SQLiteExecutor
+  private executor: SQLiteExecutorObject
   private storeName: string
 
-  constructor(executor: SQLiteExecutor, storeName: string) {
+  constructor(executor: SQLiteExecutorObject, storeName: string) {
     this.executor = executor
     this.storeName = storeName
   }
@@ -18,10 +18,10 @@ export default class Store {
     );
     `
 
-    await this.executor(init)
+    await this.executor.exec(init)
   }
 
-  async set(key: string, value: unknown) {
+  async set(key: string, value: ParamsValueType) {
     const stringValue = JSON.stringify(value)
     const insert = `
     INSERT INTO ${this.storeName} (key, value)
@@ -29,54 +29,51 @@ export default class Store {
     ON CONFLICT(key) DO UPDATE SET value = excluded.value;
     `
 
-    await this.executor(insert, [key, stringValue])
+    await this.executor.insert(insert, [key, stringValue])
   }
 
   async get<T>(key: string): Promise<T | undefined> {
     const select = `SELECT value FROM ${this.storeName} WHERE key = ?;`
-    const result = await this.executor<Array<{ value: string }>>(select, [key])
-    if (result.length === 0) {
-      return undefined
-    }
-    return JSON.parse(result[0].value) as T
+    const queryResult = await (this.executor.getUnique
+      ? this.executor.getUnique<{ value: string }>(select, key)
+      : this.executor.getMany<{ value: string }>(select, key))
+
+    let result: string | undefined
+    if (!queryResult) return undefined
+    if (Array.isArray(queryResult)) result = queryResult.at(0)?.value
+    else result = queryResult.value
+
+    return result ? (JSON.parse(result) as T) : undefined
   }
 
-  async delete<T>(key: string) {
+  async delete(key: string) {
     const del = `DELETE FROM ${this.storeName} WHERE key = ?`
-    await this.executor<T>(del, [key])
+    await this.executor.exec(del, key)
   }
 
-  async instertMultiple(items: Array<{ key: string; value: unknown }>) {
+  async instertMultiple(items: Array<{ key: string; value: ParamsValueType }>) {
     const insert = `
     INSERT INTO ${this.storeName} (key, value)
     VALUES (?, ?)
     ON CONFLICT(key) DO UPDATE SET value = excluded.value;
     `
-
-    await this.executor('BEGIN TRANSACTION;')
-
-    try {
-      for (const { key, value } of items) {
-        await this.executor(insert, [key, JSON.stringify(value)])
-      }
-      await this.executor('COMMIT;')
-    } catch (error) {
-      await this.executor('ROLLBACK;') // Ensure rollback on failure
-      throw error
-    }
+    await this.executor.batchInsert(
+      insert,
+      items.map(({ key, value }) => [key, JSON.stringify(value)])
+    )
   }
 
   async getKeys(options?: { limit: number; offset: number }): Promise<string[]> {
     const optionsString = options ? `LIMIT ${options.limit} OFFSET ${options.offset}` : ''
     const select = `SELECT key FROM ${this.storeName} ${optionsString};`
-    const result: Array<{ key: string }> = await this.executor(select)
+    const result: Array<{ key: string }> = await this.executor.getMany(select)
     return result.map(row => row.key)
   }
 
   async getAll(options?: { limit: number; offset: number }): Promise<Record<string, unknown>> {
     const optionsString = options ? `LIMIT ${options.limit} OFFSET ${options.offset}` : ''
     const select = `SELECT key, value FROM ${this.storeName} ${optionsString};`
-    const result: Array<{ key: string; value: string }> = await this.executor(select)
+    const result: Array<{ key: string; value: string }> = await this.executor.getMany(select)
     return result.reduce(
       (acc, row) => {
         acc[row.key] = JSON.parse(row.value)
@@ -88,7 +85,10 @@ export default class Store {
 
   async getSubFields<T>(prefix: string): Promise<Array<{ key: string; value: T }>> {
     const select = `SELECT key, value FROM ${this.storeName} WHERE key LIKE ?;`
-    const result: Array<{ key: string; value: T }> = await this.executor(select, [`${prefix}%`])
-    return result
+    const result: Array<{ key: string; value: string }> = await this.executor.getMany(
+      select,
+      `${prefix}%`
+    )
+    return result.map(({ key, value }) => ({ key, value: JSON.parse(value) as T }))
   }
 }
