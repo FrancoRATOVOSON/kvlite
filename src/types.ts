d345bcb78e1842f2cc27dc71d4b2db55a38de1b0
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2025-02-15 21:57:01 +0300

    feat: update types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,15 @@
 export type SQLiteExecutor = <T = unknown>(query: string, params?: unknown[]) => Promise<T>
 
+type ValueType = string | number | boolean | null
+export type ParamsValueType = ValueType | ValueType[] | Record<string, unknown>
+type ParamsType = [string, string]
+
+export type SQLiteExecutorObject = {
+  getUnique?: <T = unknown>(query: string, key: string) => Promise<T | null>
+  getMany: <T = unknown>(query: string, key?: string) => Promise<T[]>
+  insert: (query: string, params?: ParamsType) => Promise<void>
+  batchInsert: (query: string, params?: ParamsType[]) => Promise<void>
+  exec: (query: string, key?: string) => Promise<void>
+}
+
 export type ExecutorFactory<T> = (db: T) => SQLiteExecutor
