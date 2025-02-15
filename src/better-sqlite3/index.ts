5661b33aef01400d477d5f35f61804c8718058cf
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2025-02-15 22:33:49 +0300

    feat: update better-sqlite3 adapter

--- src/better-sqlite3/index.ts
+++ src/better-sqlite3/index.ts
@@ -1,22 +1,68 @@
 import BetterSQLite3 from 'better-sqlite3'
 
-import { ExecutorFactory } from '../types'
+import { ExecutorFactory, ParamsType } from '../types'
 
 export const createBetterSqliteExecutor: ExecutorFactory<BetterSQLite3.Database> = db => {
-  return <T>(query: string, params: unknown[] = []) => {
-    return new Promise<T>((resolve, reject) => {
-      try {
-        const stmt = db.prepare(query)
-        let result
-        if (query.trim().toUpperCase().startsWith('SELECT')) {
-          result = stmt.all(params)
-        } else {
-          result = stmt.run(params)
+  return {
+    getUnique: <T>(query: string, key: string) => {
+      return new Promise<T | null>((resolve, reject) => {
+        try {
+          const stmt = db.prepare<string[], T>(query)
+          const result = stmt.get(key)
+          resolve(result ?? null)
+        } catch (error) {
+          reject(error)
         }
-        resolve(result as T)
-      } catch (error) {
-        reject(error)
-      }
-    })
+      })
+    },
+    getMany: <T>(query: string, key?: string) => {
+      return new Promise<T[]>((resolve, reject) => {
+        try {
+          const stmt = db.prepare<string[], T>(query)
+          const result = key ? stmt.all(key) : stmt.all()
+          resolve(result)
+        } catch (error) {
+          reject(error)
+        }
+      })
+    },
+    insert: (query: string, params?: ParamsType) => {
+      return new Promise<void>((resolve, reject) => {
+        try {
+          const stmt = db.prepare<string[], void>(query)
+          if (params) stmt.run(...params)
+          else stmt.run()
+          resolve()
+        } catch (error) {
+          reject(error)
+        }
+      })
+    },
+    batchInsert: (query: string, params: ParamsType[]) => {
+      return new Promise<void>((resolve, reject) => {
+        try {
+          const stmt = db.prepare<string[], void>(query)
+          const insertMany = db.transaction((params: ParamsType[]) => {
+            for (const param of params) stmt.run(...param)
+          })
+          insertMany(params)
+          resolve()
+        } catch (error) {
+          reject(error)
+        }
+      })
+    },
+    exec: (query: string, key?: string) => {
+      return new Promise<void>((resolve, reject) => {
+        try {
+          const stmt = db.prepare<string[], void>(query)
+          if (key) stmt.run(key)
+          else stmt.run()
+          resolve()
+        } catch (error) {
+          reject(error)
+        }
+      })
+    }
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -2,14 +2,14 @@ export type SQLiteExecutor = <T = unknown>(query: string, params?: unknown[]) =>
 
 type ValueType = string | number | boolean | null
 export type ParamsValueType = ValueType | ValueType[] | Record<string, unknown>
-type ParamsType = [string, string]
+export type ParamsType = [string, string]
 
 export type SQLiteExecutorObject = {
   getUnique?: <T = unknown>(query: string, key: string) => Promise<T | null>
   getMany: <T = unknown>(query: string, key?: string) => Promise<T[]>
   insert: (query: string, params?: ParamsType) => Promise<void>
-  batchInsert: (query: string, params?: ParamsType[]) => Promise<void>
+  batchInsert: (query: string, params: ParamsType[]) => Promise<void>
   exec: (query: string, key?: string) => Promise<void>
 }
 
-export type ExecutorFactory<T> = (db: T) => SQLiteExecutor
+export type ExecutorFactory<T> = (db: T) => SQLiteExecutorObject
