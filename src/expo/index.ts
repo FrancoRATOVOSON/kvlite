bf91d705cd884a746fb440897c9f999277492732
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2025-02-16 10:58:57 +0300

    feat: update for expo-sqlite

--- src/expo/index.ts
+++ src/expo/index.ts
@@ -1,13 +1,28 @@
 import * as ExpoSQLite from 'expo-sqlite'
 
-import { ExecutorFactory } from '../types'
+import { ExecutorFactory, ParamsType } from '../types'
 
 export const createExpoExecutor: ExecutorFactory<ExpoSQLite.SQLiteDatabase> = db => {
-  return <T>(query: string, params: unknown[] = []) => {
-    if (query.trim().toUpperCase().startsWith('SELECT')) {
-      return db.getAllAsync(query, params as ExpoSQLite.SQLiteBindValue[]) as T
-    } else {
-      return db.runAsync(query, params as ExpoSQLite.SQLiteBindValue[]) as T
+  return {
+    getUnique: <T>(query: string, key: string) => {
+      return db.getFirstAsync<T>(query, key)
+    },
+    getMany: <T>(query: string, key?: string) => {
+      return key ? db.getAllAsync<T>(query, key) : db.getAllAsync<T>(query)
+    },
+    insert: async (query: string, params?: ParamsType) => {
+      if (params) await db.runAsync(query, ...params)
+      else await db.runAsync(query)
+    },
+    batchInsert: (query: string, params: ParamsType[]) => {
+      return db.withTransactionAsync(async () => {
+        for (const insert of params) await db.runAsync(query, ...insert)
+      })
+    },
+    exec: async (query: string, key?: string) => {
+      if (key) {
+        db.runAsync(query, key)
+      } else db.execAsync(query)
     }
   }
 }
